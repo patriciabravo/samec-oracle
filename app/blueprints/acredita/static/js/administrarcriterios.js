fb80b2ad3ef7211537ae72e0bb1ad4e7ecdc2ea9
patriciabravo <patricia.bravo@pucp.pe>
2026-06-09 15:01:04 -0500

    fix admin crtierios

--- app/blueprints/acredita/static/js/administrarcriterios.js
+++ app/blueprints/acredita/static/js/administrarcriterios.js
@@ -28,12 +28,24 @@ var CriteriosEvent = {
                 type: 'GET',
                 success: function (data) {
                     console.log(data);
-                    $('#form_edit_criterio').find('#codigo_ipress').val(data.codigo_criterio);
+                    $('#form_edit_criterio').find('#codigo_criterio').val(data.codigo_criterio);
                     $('#form_edit_criterio').find('#sel_tipo_criterio').val(data.tipo_criterio);
-
+                    $('#form_edit_criterio').find('#sel_procesoinstitucional').val(data.id_proceso).trigger('change');
+                    $('#form_edit_criterio').find('#nombre_criterio').val(data.nombre_criterio);
+                    $('#form_edit_criterio').find('#puntaje_0').val(data.puntaje_0_txt);
+                    $('#form_edit_criterio').find('#puntaje_1').val(data.puntaje_1_txt);
+                    $('#form_edit_criterio').find('#puntaje_2').val(data.puntaje_2_txt);
+                    $('#form_edit_criterio').find('#sel_nivel_i_1').val(data.nivel_i_1).trigger('change');
+                    $('#form_edit_criterio').find('#sel_nivel_i_2').val(data.nivel_i_2).trigger('change');
+                    $('#form_edit_criterio').find('#sel_nivel_i_3').val(data.nivel_i_3).trigger('change');
+                    $('#form_edit_criterio').find('#sel_nivel_i_4').val(data.nivel_i_4).trigger('change');                    
+                    $('#form_edit_criterio').find('#sel_nivel_ii_1').val(data.nivel_ii_1).trigger('change');
+                    $('#form_edit_criterio').find('#sel_nivel_ii_2').val(data.nivel_ii_2).trigger('change');
+                    $('#form_edit_criterio').find('#sel_nivel_iii_1').val(data.nivel_iii_1).trigger('change');
+                    $('#form_edit_criterio').find('#sel_aplica_essalud').val(data.aplica_essalud).trigger('change');
                 },
                 error: function () {
-                    alert("Error al cargar los datos del usuario");
+                    alert("Error al cargar los datos del Criterio");
                 }
             });
             if (mode === 'ver') {
